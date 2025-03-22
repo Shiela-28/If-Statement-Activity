b1e997cb712b57b39fc453d4af431cb4f5eea58d
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 08:56:05 +0800

    Add files via upload

--- ageCheck.js
+++ ageCheck.js
@@ -1,9 +1,11 @@
-let age =(prompt("Enter your age:"));
+// Prompt the user to enter their age.
+let age = prompt("Enter your age:"); //The prompt function is used to get input from the user.
 
-if (age < 13) {
-  console.log("You are a child.");
-} else if (age >= 13 && age <= 19) {
-  console.log("You are a teenager.");
-} else {
-  console.log("You are an adult.");
+// Check the age and display the appropriate message.
+if (age < 13) { //If the age is less than 13, the user is considered a child.
+  console.log("You are a child."); 
+} else if (age >= 13 && age <= 19) { //If the age is between 13 and 19 (inclusive), the user is considered a teenager.
+  console.log("You are a teenager."); 
+} else { //If the age is greater than 19, the user is considered an adult.
+  console.log("You are an adult."); 
 }
